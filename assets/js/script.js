05861a0bbf821353ec0bb4cf8ff2cdb7a439f9bf
Elibrer <eli.brer@gmail.com>
2023-03-28 18:39:40 +1100

    Updated recent buttons to retrieve from locally stored array.

--- assets/js/script.js
+++ assets/js/script.js
@@ -62,7 +62,22 @@ const mildMensURL = {
     head: "https://asos2.p.rapidapi.com/products/v3/detail?id=" + mildMensHeadPID + "&lang=en-AU&store=AU&sizeSchema=AU&currency=AUD",
 }
 
+let recentSearchArr = JSON.parse(localStorage.getItem("recentSearches")) || [];
 
+
+
+//On page load
+function init() { 
+
+
+
+    for (i = recentSearchArr.length - 1; i >=0; i--) {
+        var recentBtn = $("<button class='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-1'></button>");
+        recentBtn.text(recentSearchArr[i]);
+        previousSearches.prepend(recentBtn);
+    }
+
+}
 
 
 function cityTempSearch(city) {
@@ -70,7 +85,8 @@ function cityTempSearch(city) {
     let recentSearch = inputField.val();
     recentSearch = recentSearch.charAt(0).toUpperCase() + recentSearch.slice(1).toLowerCase();
 
-    
+
+
     if (!city) {
         alert('Please enter a value into the search bar.');
         return;
@@ -121,21 +137,62 @@ function displayTime() {
   
 displayTime();
 
+
+
+// function categoryItemRandomiser (clothesData) {
+//     console.log("hello");
+//     for (i = 0; i < 3; i++) {
+//         let randomIndex = Math.floor(Math.random() * recentSearchArr.length);
+
+//         var clothesContainer = document.createElement('article');
+//         var innerContainer = document.createElement('div');
+    
+//         clothesContainer.setAttribute("class", "mx-2.5 rounded-lg flex justify-center items-center w-full sm:w-2/12 bg-sky-900 text-white");
+//         innerContainer.setAttribute("class", "flex flex-wrap justify-center items-center mx-auto p-3");
+        
+    
+//         clothingOptions.append(clothesContainer);
+//         clothesContainer.append(innerContainer);
+    
+//         innerContainer.innerHTML = `
+//             <h4 class="font-bold text-sm">${clothesData.products[randomIndex].name}</h4>
+//             <a href="${clothesData.products[randomIndex].url}">
+//             <img src="https://${clothesData.media.images[0].imageUrl}" class="rounded-lg shadow-lg w-full scale-75">
+//             </a>
+//         `;
+
+
+//     }
+// }
+
+
+
 function displayClothes(clothesData) {
 
     var clothesContainer = document.createElement('article');
     var innerContainer = document.createElement('div');
 
     clothesContainer.setAttribute("class", "mx-2.5 rounded-lg flex justify-center items-center w-full sm:w-2/12 bg-sky-900 text-white");
-    innerContainer.setAttribute("class", "flex flex-wrap justify-center items-center mx-auto");
+    innerContainer.setAttribute("class", "flex flex-wrap justify-center items-center mx-auto p-3");
     
 
     clothingOptions.append(clothesContainer);
     clothesContainer.append(innerContainer);
 
+
+    //Need this to determine which of the array indexs is the en-AU one, otherwise
+    //the link won't load properly.
+    let localDataIndex = -1;
+    for (i = 0; i < clothesData.localisedData.length; i++) {
+        if (clothesData.localisedData[i].locale === "en-AU") {
+            localDataIndex = i;
+        }
+    }
+    console.log("Local data index is: " + localDataIndex);
+
     innerContainer.innerHTML = `
     <h4 class="font-bold text-sm">${clothesData.name}</h4>
-    <a href="${clothesData.localisedData[1].pdpUrl}">
+    <a href="${clothesData.localisedData[localDataIndex].pdpUrl}">
         <img src="https://${clothesData.media.images[0].url}" class="rounded-lg shadow-lg w-full scale-75">
     </a>
     `;
@@ -145,10 +202,21 @@ function displayClothes(clothesData) {
 
 
 function getRecentSearch (recentSearch) {
+    let firstRecentSearch = previousSearches.children().last();
+
+    if (recentSearchArr.length >= 5) {
+        recentSearchArr.pop();
+        firstRecentSearch.remove();
+    }
+
 
     var recentBtn = $("<button class='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-1'></button>");
     recentBtn.text(recentSearch);
     previousSearches.prepend(recentBtn);
+    recentSearchArr.unshift(recentSearch);
+
+    localStorage.setItem("recentSearches", JSON.stringify(recentSearchArr));
+
 
 }
 
@@ -169,6 +237,22 @@ function getClothesData (x) {
 
     if (x > 26) {
         console.log("Summer time!");
+
+        //Get category - to be used for phase 2 of product design
+        // fetch('https://asos2.p.rapidapi.com/products/v2/list?store=AU&offset=0&categoryId='+ mensSummerCID + '&limit=48&country=AU&sort=freshness&currency=AUD&sizeSchema=AU&lang=en-AU', options)
+        // .then(function (response) {
+        //     return response.json();
+        // })
+        // .then(function (data) {
+        //     console.log(data);
+        //     categoryItemRandomiser(data);
+
+        // })
+        // .catch((error) => {
+        //     console.error("There has been a problem with your fetch operation:");
+        // });
+
+
 
         fetch(summerMensURL.shirt, options)
         .then(function (response) {
@@ -348,5 +432,12 @@ function getClothesData (x) {
     
 
     //}
+
+
+
+    init();
+
+
+
 
 })
